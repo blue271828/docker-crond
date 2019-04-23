1c7c39bcadc290223d8f41d42dbf05e991745804
blue271828 <22131684+blue271828@users.noreply.github.com>
2019-04-23 23:03:43 +0900

    Add several test cases for Dockerfile

--- spec/Dockerfile_spec.rb
+++ spec/Dockerfile_spec.rb
@@ -1,5 +1,19 @@
 require 'spec_helper'
 
+
 describe package('tzdata') do
   it { should be_installed }
+end
+
+describe command('date +"%Z"') do
+  its(:stdout) { should match /UTC/ }
+end
+
+describe file('/entrypoint.sh') do
+  it { should exist }
+end
+
+describe command('ps') do
+  its(:stdout) { should match /entrypoint.sh/ }
+  its(:stdout) { should match /crond/ }
 end
